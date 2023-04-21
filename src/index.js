2e0c1eaaf4a5e6ee886291993aad7fa7ab7eec79
kane chau <haichau0818@gmail.com>
2023-04-21 13:28:43 +0700

    add handlebars,morgan

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').engine;
+const { config } = require('process');
+const app = express();
+const port = 3000;
+
+//http logger
+app.use(morgan('combined'));
+
+//template engine
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'./resources/views'));
+
+
+app.get('/trang-chu', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
